61444921fffbe1ae80b7e131d484e4cfffe91377
hilfor <daniel.kantorov@gmail.com>
2016-02-15 22:31:01 +0200

    First commit

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardManager : MonoBehaviour {
+
+    public GameObject _Anchor;
+    public GameObject _BinPrefab;
+    public float _DeltaX;
+    public float _DeltaY;
+
+    GameObject[,] _board;
+
+    // Use this for initialization
+    void Start()
+    {
+        _board = new GameObject[4, 4];
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                Instantiate(_BinPrefab, _Anchor.transform.position + new Vector3(i * _DeltaX, j * _DeltaY, 0), Quaternion.identity);
+            }
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
